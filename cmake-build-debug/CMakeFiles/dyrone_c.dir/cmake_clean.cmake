1497660ddd64f67a9e943d9797a7e79d2eeb88f4
Dyrone Teng <tenglong.tl@alibaba-inc.com>
2020-08-16 22:53:00 +0800

    Chapter 1: first hello world
    Signed-off-by: Dyrone Teng <tenglong.tl@alibaba-inc.com>

---
+++ cmake-build-debug/CMakeFiles/dyrone_c.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dyrone_c.dir/chapter_1/main.c.o"
+  "dyrone_c"
+  "dyrone_c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dyrone_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
